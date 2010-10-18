e151c78ee662acf51a9b7cdc2dee6bb704d1bcce
Stephen Johnson <srj15@case.edu>
2010-10-17 23:18:50 -0400

    Bare bones script that does nothing
    Signed-off-by: Stephen Johnson <srj15@case.edu>

---
+++ xeger.py
@@ -0,0 +1,16 @@
+import sys
+import difflib
+
+class Xeger(object):
+    """Find regexes matching sets of sequences"""
+    def __init__(self, sequences):
+        self.sequences = sequences
+    
+    def regexes(self):
+        return []
+        
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as f:
+        xe = Xeger(f.lines())
+        print '\n'.join(xe.regexes())
